30dee582a27dcc9a5adf3f81739354f74087411d
Bruno Alencar <brunobubat@yahoo.com.br>
2023-12-17 13:58:27 -0300

    home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,60 @@
 'use client'
 
+import Link from "next/link";
 import { useGlobalContext } from "../context/store";
+import Image from "next/image";
+import { faDollarSign, faFileContract, faRoute, faSchool, faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Home() {
   const { user } = useGlobalContext();
-  if (user?.type === 'DRIVER') {
-    return (
-      <div className="container mt-5">
-        <div className="row">
-          <div className="col-sm-6 mb-3 mb-sm-0">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Cadastrar escolas</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="/schools" className="btn btn-primary">Ir para serviço</a>
-              </div>
-            </div>
-          </div>
-          <div className="col-sm-6">
-            <div className="card">
-              <div className="card-body">
-                <h5 className="card-title">Special title treatment</h5>
-                <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" className="btn btn-primary">Ir para serviço</a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    )
+  const services = {
+    driver: [
+      { name: '/schools', icon: faSchool, title: 'Escolas' },
+      { name: '/routes', icon: faRoute, title: 'Rotas' },
+      { name: '/parents', icon: faUsers, title: 'Responáveis' },
+      { name: '/finances', icon: faDollarSign, title: 'Financeiro' }
+    ],
+    parent: [
+      { name: '/contracts', icon: faFileContract, title: 'Contratos' },
+    ]
   }
+
+  const currServices = user?.type === 'DRIVER' ? services.driver : services.parent;
+
+  return (
+    <div className="container mt-5">
+      <div className="text-center mb-5">
+        <h1>{user?.type === 'DRIVER' ? 'Motorista' : 'Responsável'}</h1>
+        <hr />
+        <Image
+          src="/dark_logo.png"
+          width={500}
+          height={142}
+          alt="Picture of the author"
+        />
+        <hr />
+      </div>
+      <div className="row g-4 justify-content-center">
+        {currServices.map((service, index) => (
+          <div key={index} className="col-md-6 col-lg-3">
+            <div className="card h-100 border-0 shadow">
+              <div className="card-body text-center">
+                <h5 className="card-title mb-3">
+                  <FontAwesomeIcon
+                    icon={service.icon}
+                  />
+                  &nbsp;&nbsp;
+                  {service.title}
+                </h5>
+                <Link href={service.name} className="btn btn-primary">
+                  Ir para serviço
+                </Link>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
